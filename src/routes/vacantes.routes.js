ecb68a0000af448f2231078282c198f6a5e76044
Francisco Javier Gutierrez Herculano <chinopaco.05@gmail.com>
2026-03-06 00:53:26 -0600

    .

--- src/routes/vacantes.routes.js
+++ src/routes/vacantes.routes.js
@@ -4,16 +4,13 @@ const vacantesController = require('../controllers/vacantes.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 const roleMiddleware = require('../middlewares/role.middleware');
 
-// Public routes
 router.get('/', vacantesController.getVacantes);
 
-// 'mis-vacantes' must go before /:id to avoid confusing 'mis-vacantes' as an ID param
 router.get('/mis-vacantes', authMiddleware, roleMiddleware(['empleador', 'admin']), vacantesController.getMisVacantes);
 router.get('/:id', vacantesController.getVacanteById);
 
-// Protected routes (Create, Update, Delete)
 router.post('/', authMiddleware, roleMiddleware(['empleador', 'admin']), vacantesController.createVacante);
-router.put('/:id', authMiddleware, vacantesController.updateVacante); // Inside checks if owner || admin
-router.delete('/:id', authMiddleware, vacantesController.deleteVacante); // Inside checks if owner || admin
+router.put('/:id', authMiddleware, vacantesController.updateVacante);
+router.delete('/:id', authMiddleware, vacantesController.deleteVacante);
 
 module.exports = router;
