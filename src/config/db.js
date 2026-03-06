74e2166ce7d1dacecb83e9561773a0d2d5bf26ca
Francisco Javier Gutierrez Herculano <chinopaco.05@gmail.com>
2026-03-06 00:41:23 -0600

    pg

--- src/config/db.js
+++ src/config/db.js
@@ -1,27 +1,31 @@
 const { Sequelize } = require('sequelize');
+const pg = require('pg');
+pg.defaults.ssl = { rejectUnauthorized: false };
 require('dotenv').config();
 
 const sequelize = process.env.DATABASE_URL
   ? new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    logging: false, // Cambia a console.log para ver transacciones
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  })
-  : new Sequelize(
-    process.env.DATABASE_NAME,
-    process.env.DATABASE_USER,
-    process.env.DATABASE_PASSWORD,
-    {
-      host: process.env.DATABASE_HOST,
-      port: process.env.DATABASE_PORT || 5432,
       dialect: 'postgres',
-      logging: false,
-    }
-  );
+      dialectModule: pg, // <--- ESTO ES LO QUE FALTABA
+      logging: false, 
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DATABASE_NAME,
+      process.env.DATABASE_USER,
+      process.env.DATABASE_PASSWORD,
+      {
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT || 5432,
+        dialect: 'postgres',
+        dialectModule: pg,
+        logging: false,
+      }
+    );
 
 module.exports = sequelize;
