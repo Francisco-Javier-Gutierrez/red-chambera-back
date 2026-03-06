beb101743bbcc3a94252cf68389a7d41b21dd7e0
Francisco Javier Gutierrez Herculano <chinopaco.05@gmail.com>
2026-03-06 00:11:38 -0600

    Fix sequelize config for vercel

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,27 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  {
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    logging: false, // Cambia a console.log para ver las transacciones SQL
-  }
-);
+    logging: false, // Cambia a console.log para ver transacciones
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  })
+  : new Sequelize(
+    process.env.DATABASE_NAME,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT || 5432,
+      dialect: 'postgres',
+      logging: false,
+    }
+  );
 
 module.exports = sequelize;
