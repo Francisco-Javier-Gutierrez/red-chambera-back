9ae7c3a14d3faa90fc68687e94109e45b6bceb62
Francisco Javier Gutierrez Herculano <chinopaco.05@gmail.com>
2026-03-06 01:03:38 -0600

    .

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('./src/models');
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // En producción cambia esto a la URL de tu frontend (ej. 'http://localhost:5173')
+    origin: ['http://localhost:5173', "https://red-chambera-front.vercel.app"], // En producción cambia esto a la URL de tu frontend (ej. 'http://localhost:5173')
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
